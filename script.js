2deac8a2ab5d1aa20b60b474383d9e918256e005
Carter Shean <cartershean2@gmail.com>
2019-09-25 10:41:57 -0400

    update to use windows line endings

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ function generateCSV() {
   let rowNum = document.getElementById("numRows").value;
   let csv = "";
   let randomWords = ["Molly", "John", "Joe", "Pedro", "Daequan", "Latasha", "Maria", "Andrew", "Ricardo", "Fred", "Jane", "Sofia", "Isabella", "Willie", "Vladimir"];
+
   for(let i = 0; i < rowNum; i++) {
     let newRow = "";
     for(let j = 0; j < colNum; j++) {
       newRow += randomWords[Math.floor(Math.random() * 15)] + ", ";
     }
-    csv += newRow + "\n";
+    csv += newRow + "\r\n";
   }
 
   let fileName = 'generated.csv';
